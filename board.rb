bd85fa05e19e301bf3e81b98f8a92c9e017d71c8
App Academy Student <student@appacademy.io>
2017-01-26 18:07:24 -0800

    did half of minesweeper

--- board.rb
+++ board.rb
@@ -1,4 +1,4 @@
-require 'tile'
+require_relative 'tile'
 
 class Board
   attr_reader :size, :bomb_count, :grid
@@ -14,7 +14,39 @@ class Board
     bomb_count.times { temp_grid << Tile.new(true) }
     (size**2 - bomb_count).times { temp_grid << Tile.new }
     @grid = temp_grid.shuffle.each_slice(size).to_a
+    assign_bomb_count
   end
+
+  def assign_bomb_count
+    grid.each.with_index do |row, i|
+      row.each.with_index do |tile, j|
+        next if tile.is_a_bomb
+        tile.bomb_count = count_bombs
+      end
+    end
+  end
+
+  def count_bombs
+    get_my_neighbor_positions([i, j]).select do |pos|
+      board[pos].is_a_bomb
+    end.count
+  end
+
+  def get_my_neighbor_positions(pos)
+    positions = []
+
+    i, j = pos
+    (i-1..i+1).each do |i2|
+      (j-1..j+1).each do |j2|
+        positions << [i2, j2] unless [i2, j2] == pos
+      end
+    end
+    positions.select do |i3, j3|
+      i3.between?(0, size - 1) && j3.between? (0, size - 1)
+    end
+  end
+
+
 
   def [](pos)
     x, y = pos
@@ -42,7 +74,7 @@ class Board
         "*"
       end
     end
-    puts display.each_slice(size).to_a
+    display.each_slice(size).to_a.each{|row| p row.join(" ")}
   end
 
 end
